49dc380c1bf9603cf8c3ec55437de2e06d2c3e54
Artsiom <pelemen007@mail.ru>
2017-04-02 22:35:34 +0300

    Add files via upload

--- js/stat.js
+++ js/stat.js
@@ -2,33 +2,31 @@
 
 window.renderStatistics = function (ctx, names, times) {
 
-//Отрисовка тёмного прямоугольника тени
+// Отрисовка тёмного прямоугольника тени
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
 
-//Очистка области под белый треугольник
+// Очистка области под белый треугольник
   ctx.clearRect(100, 10, 420, 270);
 
-//Отрисовка белого прямоугольника
+// Отрисовка белого прямоугольника
   ctx.fillStyle = 'white';
   ctx.fillRect(100, 10, 420, 270);
 
-//Пишем текст
+// Пишем текст
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура, вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
-//Выводим гистограмму
+// Выводим гистограмму
 
   var max = -1;
-  var maxIndex = -1;
 
   for (var i = 0; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
-      maxIndex = i;
     }
   }
 
